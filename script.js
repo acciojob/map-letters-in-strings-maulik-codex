6562edd3048794e440762dae629160227c69bc66
Maulik Chhansiya <maulikchhansiya@gmail.com>
2025-06-17 22:45:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//your JS code here. If required.
+function mapLetters(word) {
+  const result = {};
+  
+  for (let i = 0; i < word.length; i++) {
+    const letter = word[i];
+    const sym = Symbol.for(letter);  // Use Symbol.for to get a symbol for that letter
+    
+    if (!result[sym]) {
+      result[sym] = [];
+    }
+    result[sym].push(i);
+  }
+  
+  return result;
+}
